108f64f72ef983b35c8329b50a98b16d119dc7ed
Christian I. Nilsson <nikize@gmail.com>
2023-06-01 17:22:37 +0200

    .NET Tests some Validation of data

---
+++ NET/Swedishbankers.Bank.Tests/Helpers.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework.Constraints;
+
+namespace Swedishbankers.Bank.Tests;
+
+internal static class Helpers
+{
+    public static DirectoryInfo GetBaseDir()
+    {
+        var ncruncProj = Environment.GetEnvironmentVariable("NCrunch.OriginalProjectPath");
+        var ncruncProjDi = ncruncProj == null ? null : new FileInfo(ncruncProj).Directory;
+        var di = ncruncProjDi ?? new DirectoryInfo(Directory.GetCurrentDirectory());
+        while (true)
+        {
+            Console.WriteLine($" {nameof(GetBaseDir)} Looking for Data dir .. {di}");
+            var diData = di.EnumerateDirectories("Data").FirstOrDefault();
+            if (diData != null)
+                return diData;
+            di = di.Parent;
+            if (di == null)
+                throw new Exception("Data spath not found");
+        }
+    }
+
+    public static Constraint ContainsAnyUmlaut()
+    {
+        Constraint x = Does.Contain("Å");
+        foreach (var c in "ÅÄÖåäö")
+            x = x.Or.Contain(c);
+        return x;
+    }
+}
---
+++ NET/Swedishbankers.Bank.Tests/SimpleValidationsTests.cs
@@ -0,0 +1,26 @@
+namespace Swedishbankers.Bank.Tests;
+
+public class SimpleValidationsTests
+{
+    [Test]
+    public void SourcePsvHasDataTest()
+    {
+        Assert.That(Data.Banks.SourcePsv, Is.Not.Null.Or.Empty,
+            Data.Banks.SourcePsv);
+    }
+
+    [TestCase("source.psv")]
+    public void SourceFileHasCorrectEncoding(string fileName)
+    {
+        var fi = Helpers.GetBaseDir().EnumerateFiles(fileName).First();
+        var fileData = File.ReadAllText(fi.FullName, System.Text.Encoding.UTF8);
+        Assert.That(fileData, Helpers.ContainsAnyUmlaut(),
+            fileData);
+    }
+
+    [Test]
+    public void SourcePsvHasCorrectEncoding()
+    {
+        Assert.That(Data.Banks.SourcePsv, Helpers.ContainsAnyUmlaut());
+    }
+}
