20b85c9837f61a797199876bc5086b6c35574430
Christian I. Nilsson <nikize@gmail.com>
2023-06-16 21:33:59 +0200

    .NET Bankinfrastruktur.Data GenerateDocumentationFile

--- NET/Bankinfrastruktur.Data/CheckDigitTypeType.cs
+++ NET/Bankinfrastruktur.Data/CheckDigitTypeType.cs
@@ -6,6 +6,6 @@ public enum CheckDigitTypeType
     /// <summary>For <see cref="AccountTypeType.Type2"/> mod10 on account part</summary>
     Comment1 = 1,
     Comment2 = 2,
-    /// <summary>Only <see cref="AccountTypeType.Type2"/> Swedbank & plusgirot mod10</summary>
+    /// <summary>Only <see cref="AccountTypeType.Type2"/> Swedbank &amp; plusgirot mod10</summary>
     Comment3 = 3,
 }
