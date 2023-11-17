41fcfe196418d9361d8d9fd6c2b9fb2672a129e6
Christian I. Nilsson <nikize@gmail.com>
2023-11-17 16:55:35 +0100

    .NET WaybackSnapshot handle exceptions

--- NET/Helpers/WaybackSnapshot.cs
+++ NET/Helpers/WaybackSnapshot.cs
@@ -19,7 +19,20 @@ public class WaybackSnapshot
 
     public static async Task<WaybackSnapshot?> GetArchiveData(Uri url, string? filterMime = "application/pdf")
     {
-        using var s = await HttpClient.GetStreamAsync($"{ArchiveBase}cdx/search/cdx?fl=timestamp,original,mimetype,digest,length&from={DateTime.Now.Year - 1}&filter=statuscode:200&collapse=digest&url={url}");
+        var archiveUrl = new Uri($"{ArchiveBase}cdx/search/cdx?fl=timestamp,original,mimetype,digest,length&from={DateTime.Now.Year - 1}&filter=statuscode:200&collapse=digest&url={url}");
+        try
+        {
+            return await GetParseArchiveInner(archiveUrl, filterMime);
+        }
+        catch (HttpRequestException ex)
+        {
+            throw new Exception($"Fetch Fail {archiveUrl}, {ex.Message}", ex);
+        }
+    }
+    
+    private static async Task<WaybackSnapshot?> GetParseArchiveInner(Uri archiveUrl, string? filterMime)
+    {
+        using var s = await HttpClient.GetStreamAsync(archiveUrl);
         using var tr = new StreamReader(s);
         string? line;
         WaybackSnapshot? lastSnap = null;
