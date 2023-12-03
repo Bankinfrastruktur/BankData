12bef83d80593e53da591474944395fa5c94e2da
Christian I. Nilsson <nikize@gmail.com>
2023-12-03 22:39:59 +0100

    .NET Tests EnsureData allow for pipe separation in source files
    ParsePdfs will use pipe separation instead of spaces which will make parsing easier

--- NET/Bankinfrastruktur.Tests/DataValidationTests.cs
+++ NET/Bankinfrastruktur.Tests/DataValidationTests.cs
@@ -74,6 +74,9 @@ public class DataValidationTests
         });
     }
 
+    private static bool IsSep(char c) => c == ' ' || c == '|'
+        || CharUnicodeInfo.GetUnicodeCategory(c) == UnicodeCategory.SpaceSeparator;
+
     private static IEnumerable<string> StripToBankgirotTypComment(string data) =>
         Helpers.GetLines(data)
         .Select(l => l.Split('|'))
@@ -86,10 +89,6 @@ public class DataValidationTests
 
     private static IEnumerable<string[]> ParseBankgirotLine(string l)
     {
-        var s = l.Split('|');
-        if (s.Length != 1)
-            return new[] { s };
-
         // Name, clearing - clearing format comment
         var i = l.Length - 1;
         var curS = new StringBuilder(i);
@@ -100,12 +99,8 @@ public class DataValidationTests
         while (i >= 0)
         {
             var c = l[i--];
-            var ctc = CharUnicodeInfo.GetUnicodeCategory(c);
-            var isSpace = ctc == UnicodeCategory.SpaceSeparator;
-            if (ctc == UnicodeCategory.DashPunctuation &&
-                c != '-') c = '-';
-            //Console.WriteLine($"{c} {ctc} {parts.Count} {curS}");
-            if (isSpace && parts.Count <= 1)
+            var isSepCandidate = IsSep(c);
+            if (isSepCandidate && parts.Count <= 1)
             {
                 if (parts.Count == 0)
                 {
@@ -118,15 +113,15 @@ public class DataValidationTests
             }
             if (parts.Count == 1)
             {
-                var expectedChars = formatLength == 0 ? new object[] { 'C' } :
-                    prefixLength == 0 ? new object[] { 'x', '0' } :
-                    new object[] { '0' };
+                var expectedChars = formatLength == 0 ? new char[] { 'C' } :
+                    prefixLength == 0 ? new char[] { 'x', '0' } :
+                    new char[] { '0' };
                 Assert.That(c, Is.AnyOf(expectedChars), $"Unexpected format char, prev {curS} from {l}");
                 if (c == 'C' || c == 'x')
                     formatLength++;
                 else prefixLength++;
             }
-            else if (isSpace && parts.Count == 2)
+            else if (isSepCandidate && parts.Count == 2)
             {
                 var cnxt = l[i];
                 var ctcnxt = CharUnicodeInfo.GetUnicodeCategory(cnxt);
@@ -160,26 +155,26 @@ public class DataValidationTests
             .Replace("/Plusgirot", " (Plusgirot)")
             .Replace(" Bank AB", " Bank");
         var normParts = new[] { null, null, name, $"Type{typ}", $"Comment{parts[3]}", $"{formatLength}" };
-        var ranges = new[] { Tuple.Create(clrStart, clrEnd) };
+        var ranges = new[] { (clrStart, clrEnd) };
         if (clearinRange[4] == '/')
-            ranges = new[] { Tuple.Create(clrStart, clrStart), 
-                Tuple.Create(clrEnd, clrEnd) };
+            ranges = new[] { (clrStart, clrStart), 
+                (clrEnd, clrEnd) };
         foreach (var splClr in new[] { 3300, 3782 })
         {
             if (clrStart < splClr && splClr < clrEnd)
             {
-                ranges = new[] { Tuple.Create(clrStart, splClr - 1),
-                Tuple.Create(splClr + 1, clrEnd) };
+                ranges = new[] { (clrStart, splClr - 1),
+                (splClr + 1, clrEnd) };
                 normParts[2] = name.Replace($" (exkl. personkonton, cl {splClr})", string.Empty);
             }
         }
 
         return ranges.SelectMany(r =>
         {
-            normParts[0] = $"{r.Item1}";
-            normParts[1] = $"{r.Item2}";
+            normParts[0] = $"{r.clrStart}";
+            normParts[1] = $"{r.clrEnd}";
             // Console.WriteLine($"{string.Join('|', normParts)} src: {string.Join('|', parts)}");
-            return ParseIbanBicLine(string.Join('|', normParts));
+            return new[] { normParts as string[] };
         });
     }
 
@@ -215,14 +210,14 @@ public class DataValidationTests
         .Select(s => string.Join("|", s[0], s[1], s[2], s[3], s[4], s[7])); // , s[6]
 
     // Vi behöver dela upp och städa en del av datan ifrån PDFen
-    private static readonly Dictionary<int, Tuple<string, string[]>> TextCleaners = new()
+    private static readonly Dictionary<int, (string, string[])> TextCleaners = new()
     {
-        {3000, Tuple.Create("3000-3399, (exkl. personkto, cl nr 3300) ", new[] { "3000-3299 ", "3301-3399 " })},
-        {3300, Tuple.Create("3300 (personkto) ",  new[] { "3300-3300 "})},
-        {3410, Tuple.Create("3410-4999, (exkl. personkto, cl nr 3782) ", new[] { "3410-3781 ", "3783-3999 ", "4000-4999 " })}, // 3 och 4 har olika kommentarer
-        {3782, Tuple.Create("3782 (personkto) ", new[] { "3782-3782 " })},
-        {9190, Tuple.Create(" DnB NOR filial ", new[] { " DnB Bank " })},
-        {9280, Tuple.Create(" Resurs Bank AB ", new[] { " Resurs Bank " })},
+        {3000, ("3000-3399, (exkl. personkto, cl nr 3300)", new[] { "3000-3299", "3301-3399" })},
+        {3300, ("3300 (personkto)",  new[] { "3300-3300"})},
+        {3410, ("3410-4999, (exkl. personkto, cl nr 3782)", new[] { "3410-3781", "3783-3999", "4000-4999" })}, // 3 och 4 har olika kommentarer
+        {3782, ("3782 (personkto)", new[] { "3782-3782" })},
+        {9190, (" DnB NOR filial", new[] { " DnB Bank" })},
+        {9280, (" Resurs Bank AB", new[] { " Resurs Bank" })},
     };
 
     private static readonly Dictionary<int, string> ForceMethod = new()
@@ -232,14 +227,6 @@ public class DataValidationTests
 
     private static IEnumerable<string[]> ParseIbanBicLine(string l)
     {
-        var s = l.Split('|');
-        if (s.Length != 1)
-            return new[] { s };
-
-        // skip wildcard line
-        if (l[0] == '*')
-            return Array.Empty<string[]>();
-
         int i = 0;
         var clearingStart = Convert.ToInt32(l[..(i += 4)]);
         if (TextCleaners.TryGetValue(clearingStart, out var cleanThis) &&
@@ -256,26 +243,26 @@ public class DataValidationTests
         }
         if (l[i] == '*')
             i++; // ignore notes *Swedbank har meddelat att serien utgår i samband med att Dataclearingen avvecklas.
-        if (l[i++] != ' ')
-            throw new FormatException($"Expected Space at {i-1} (Between clearing end and IbanId) on {l}");
+        if (!IsSep(l[i++]))
+            throw new FormatException($"Expected Pipe at {i-1} (Between clearing end and IbanId) on {l}");
         var ibanId = Convert.ToInt32(l.Substring(i, 3));
         i += 3;
-        if (l[i++] != ' ')
-            throw new FormatException($"Expected Space at {i - 1} (Between IbanId and BIC) on {l}");
+        if (!IsSep(l[i++]))
+            throw new FormatException($"Expected Pipe at {i - 1} (Between IbanId and BIC) on {l}");
         var bic = l.Substring(i, 8);
         i += 8;
-        if (l[i++] != ' ')
-            throw new FormatException($"Expected Space at {i - 1} (Between BIC and name) on {l}");
+        if (!IsSep(l[i++]))
+            throw new FormatException($"Expected Pipe at {i - 1} (Between BIC and name) on {l}");
         var name = l[i..];
         // TODO check for last diggit indicating method
         var method = name[^2..];
-        if (method[0] == ' ')
+        if (IsSep(method[0]))
         {
             method = method[1..];
             name = name[..^2];
         }
         else method = ForceMethod.TryGetValue(clearingStart, out var forceMethod) ? forceMethod : "?";
-        return ParseIbanBicLine($"{clearingStart}|{clearingEnd}|{ibanId}|{bic}|{name}|{method}");
+        return new[] { new[] { $"{clearingStart}", $"{clearingEnd}", $"{ibanId}", bic, name, method } };
     }
 
     private static IEnumerable<string> ParseIbanBicText(string data)
